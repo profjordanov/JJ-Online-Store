c366f91b7c83ee4890adf578ef092da66aab0df8
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-13 00:34:45 +0300

    QueryableMappingExtensions class added

--- JjOnlineStore.Common/Mapping/QueryableMappingExtensions.cs
+++ JjOnlineStore.Common/Mapping/QueryableMappingExtensions.cs
@@ -1,7 +1,34 @@
-﻿namespace JjOnlineStore.Common.Mapping
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper.QueryableExtensions;
+
+namespace JjOnlineStore.Common.Mapping
 {
-    public class QueryableMappingExtensions
+    public static class QueryableMappingExtensions
     {
-        
+        public static IQueryable<TDestination> To<TDestination>(
+            this IQueryable source,
+            params Expression<Func<TDestination, object>>[] membersToExpand)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return source.ProjectTo(membersToExpand);
+        }
+
+        public static IQueryable<TDestination> To<TDestination>(
+            this IQueryable source,
+            object parameters)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return source.ProjectTo<TDestination>(parameters);
+        }
     }
 }
