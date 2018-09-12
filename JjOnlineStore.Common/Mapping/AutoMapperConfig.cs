0d36868f719afb00e5749a67d8cca6659cb28a00
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-13 00:44:22 +0300

    AutoMapperConfig class added

---
+++ JjOnlineStore.Common/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace JjOnlineStore.Common.Mapping
+{
+    public static class AutoMapperConfig
+    {
+        private static IEnumerable<IHaveCustomMappings> GetCustomMappings(IEnumerable<Type> types)
+        {
+            var customMaps = from t in types
+                             from i in t.GetTypeInfo().GetInterfaces()
+                             where typeof(IHaveCustomMappings).GetTypeInfo().IsAssignableFrom(t) &&
+                                   !t.GetTypeInfo().IsAbstract &&
+                                   !t.GetTypeInfo().IsInterface
+                             select (IHaveCustomMappings)Activator.CreateInstance(t);
+
+            return customMaps;
+        }
+    }
+}
---
+++ JjOnlineStore.Common/Mapping/TypesMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JjOnlineStore.Common.Mapping
+{
+    internal class TypesMap
+    {
+        public Type Source { get; set; }
+
+        public Type Destination { get; set; }
+    }
+}
