f9110fde21d64afd75e3457e2612731f096367f7
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-25 22:55:21 +0300

    Flags attribure added to Enumeration

--- Data/JjOnlineStore.Data.Entities/Enumeration/ProductType.cs
+++ Data/JjOnlineStore.Data.Entities/Enumeration/ProductType.cs
@@ -1,9 +1,12 @@
-﻿namespace JjOnlineStore.Data.Entities.Enumeration
+﻿using System;
+
+namespace JjOnlineStore.Data.Entities.Enumeration
 {
+    [Flags]
     public enum ProductType
     {
         Ordinary = 1,
-        New = 2,
-        OnSale = 3
+        New      = 2,
+        OnSale   = 4
     }
 }
--- Data/JjOnlineStore.Data.Entities/Enumeration/Size.cs
+++ Data/JjOnlineStore.Data.Entities/Enumeration/Size.cs
@@ -1,10 +1,13 @@
-﻿namespace JjOnlineStore.Data.Entities.Enumeration
+﻿using System;
+
+namespace JjOnlineStore.Data.Entities.Enumeration
 {
+    [Flags]
     public enum Size
     {
-        S = 1,
-        M = 2,
-        L = 3,
-        XL = 4
+        S  = 1,
+        M  = 2,
+        L  = 4,
+        XL = 8
     }
 }
