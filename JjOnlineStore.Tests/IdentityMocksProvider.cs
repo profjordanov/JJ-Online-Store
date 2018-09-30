077b8885d4c56c6322659bb6fd9b4bdffc633e14
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-30 15:38:45 +0300

    IdentityMocksProvider added

---
+++ JjOnlineStore.Tests/IdentityMocksProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using JjOnlineStore.Data.Entities;
+using Moq;
+
+namespace JjOnlineStore.Tests
+{
+    public static class IdentityMocksProvider
+    {
+        public static Mock<UserManager<ApplicationUser>> GetMockUserManager()
+        {
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            return new Mock<UserManager<ApplicationUser>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+        }
+    }
+}
