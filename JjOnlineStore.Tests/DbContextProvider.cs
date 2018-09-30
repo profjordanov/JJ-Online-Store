c1d8639bb32f229c2f650c2044f4736bc0032f8f
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-30 15:35:27 +0300

    DbContextProvider added

---
+++ JjOnlineStore.Tests/DbContextProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JjOnlineStore.Data.EF;
+
+namespace JjOnlineStore.Tests
+{
+    public static class DbContextProvider
+    {
+        public static JjOnlineStoreDbContext GetInMemoryDbContext()
+            => new JjOnlineStoreDbContext(new DbContextOptionsBuilder<JjOnlineStoreDbContext>()
+                .UseInMemoryDatabase("Tests").Options);
+    }
+}
