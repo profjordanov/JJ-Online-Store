5e3c47c2a67d470aeecca481da42eaa960a5a52f
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-06 10:34:51 +0300

    Admin Categories Controller added

---
+++ Web/JjOnlineStore.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JjOnlineStore.Web.Areas.Admin.Controllers
+{
+    public class CategoriesController : BaseAdminController
+    {
+
+    }
+}
