46a4e5e26fa36dedbb4e1b9d0f03fee2861ba09b
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-20 14:00:22 +0300

    all products - backend

---
+++ Services/JjOnlineStore.Services.Core/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JjOnlineStore.Common.ViewModels.Products;
+
+namespace JjOnlineStore.Services.Core
+{
+    public interface IProductsService
+    {
+        Task<IEnumerable<ProductViewModel>> AllWithoutDeletedAsync();
+    }
+}
