47a764a99657a7361224fc2866fa73a3d0a204e5
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-27 09:23:07 +0300

    Shopping Cart & Cart Item View Models

---
+++ JjOnlineStore.Common/ViewModels/ShoppingCarts/CartVm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using JjOnlineStore.Common.ViewModels.CartItems;
+
+namespace JjOnlineStore.Common.ViewModels.ShoppingCarts
+{
+    public class CartVm
+    {
+        public long Id { get; set; }
+
+        public IEnumerable<CartItemVm> CartItems { get; set; }
+
+        public decimal TotalSum { get; }
+    }
+}
---
+++ Web/JjOnlineStore.Web.Infrastructure/Mappings/ShoppingCartMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JjOnlineStore.Common.ViewModels.ShoppingCarts;
+using JjOnlineStore.Data.Entities;
+
+namespace JjOnlineStore.Web.Infrastructure.Mappings
+{
+    public class ShoppingCartMapping : Profile
+    {
+        public ShoppingCartMapping()
+        {
+            CreateMap<Cart, CartVm>(MemberList.Destination);
+        }
+    }
+}
