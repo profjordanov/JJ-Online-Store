c4bd91427356673abeb4a5b880f9b5d949f24df1
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-28 11:00:36 +0200

    374 front end changes

---
+++ Web/JjOnlineStore.Web/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JjOnlineStore.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class OrdersController : BaseController
+    {
+        /// GET: /Orders/Index
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
