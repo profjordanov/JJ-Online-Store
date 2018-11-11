b1d3c23ebd28b22704431f90fcdaaa3843a44583
Jordan Jordanov <d_yugioh@abv.bg>
2018-11-11 20:46:31 +0200

    front end work over prifle page

--- Web/JjOnlineStore.Web/Controllers/UsersController.cs
+++ Web/JjOnlineStore.Web/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JjOnlineStore.Web.Controllers
 {
-    public class UsersController : Controller
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class UsersController : BaseController
     {
         public async Task<IActionResult> Profile(string username)
         {
