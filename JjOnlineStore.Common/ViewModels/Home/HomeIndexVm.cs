b1c3c8d4e57c7154d3dc6853427e26fe01778758
Jordan Jordanov <d_yugioh@abv.bg>
2018-11-17 14:39:41 +0200

    Home view almost ready

---
+++ JjOnlineStore.Common/ViewModels/Home/HomeIndexVm.cs
@@ -0,0 +1,13 @@
+﻿using JjOnlineStore.Common.ViewModels.Products;
+
+using System.Collections.Generic;
+
+namespace JjOnlineStore.Common.ViewModels.Home
+{
+    public class HomeIndexVm
+    {
+        public IEnumerable<ProductViewModel> NewItems { get; set; }
+        public IEnumerable<ProductViewModel> OnSaleItems { get; set; }
+        public IEnumerable<ProductViewModel> TopSellingItems { get; set; }
+    }
+}
--- Web/JjOnlineStore.Web/Controllers/HomeController.cs
+++ Web/JjOnlineStore.Web/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using JjOnlineStore.Common.ViewModels.Home;
+using JjOnlineStore.Services.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JjOnlineStore.Web.Controllers
 {
     public class HomeController : BaseController
     {
-        public IActionResult Index()
+        private readonly IProductsService _productsService;
+
+        public HomeController(IProductsService productsService)
         {
-            return View();
+            _productsService = productsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomeIndexVm
+            {
+                NewItems = await _productsService.AllWithoutDeletedAsync(),
+                OnSaleItems = await _productsService.AllWithoutDeletedAsync(),
+                TopSellingItems = await _productsService.AllWithoutDeletedAsync()
+            };
+
+            return View(model);
         }
 
         public IActionResult About()
