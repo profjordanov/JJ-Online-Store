b175be1db13faaccbe478a2971df9725f8f688e1
Jordan Jordanov <d_yugioh@abv.bg>
2018-11-24 16:10:47 +0200

    work in progress

--- Web/JjOnlineStore.Web/wwwroot/js/Business/orders.index.page.js
+++ Web/JjOnlineStore.Web/wwwroot/js/Business/orders.index.page.js
@@ -1,4 +1,42 @@
-﻿$("#show-delivery-methods").click(function() {
-    $("#delivery-methods-partial").removeClass("hidden");
-    $("#payment-methods-partial").addClass("hidden");
+﻿//Order Index Partial Screens
+const $paymentMethodsPartial = $("#payment-methods-partial");
+const $deliveryMethodsPartial = $("#delivery-methods-partial");
+
+$("#show-delivery-methods").click(function () {
+    $paymentMethodsPartial.fadeOut("slow", function () {
+        $deliveryMethodsPartial.fadeIn("slow");
+        $deliveryMethodsPartial.removeClass("hidden");
+    });
 });
+
+$("#show-payment-methods").click(function () {
+    $deliveryMethodsPartial.fadeOut("slow", function () {
+        $paymentMethodsPartial.fadeIn("slow");
+    });
+});
+
+$("#submit-order-form").click(function () {
+    if (checkOrderFormInputs()) {
+        $("#create-order-form").submit();
+    } else {
+        $.fancybox("Some of the required fields are not filled." +
+            "Please, fill in all fields.");
+    }
+});
+
+
+
+function checkOrderFormInputs() {
+    var isValid = true;
+    $("input").filter("[required]").each(function () {
+        if ($(this).val() === "") {
+            $("#submit-order-form").prop("disabled", true);
+            isValid = false;
+            return false;
+        }
+    });
+    if (isValid) {
+        $("#submit-order-form").prop("disabled", false);
+    }
+    return isValid;
+}
