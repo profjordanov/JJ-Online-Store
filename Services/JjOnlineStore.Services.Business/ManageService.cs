6ac7ef1c77cafa05ad9870c9a7c0df46e06edc30
Jordan Jordanov <d_yugioh@abv.bg>
2018-11-26 19:55:04 +0200

    Manage Controller & Service added.

---
+++ Services/JjOnlineStore.Services.Business/ManageService.cs
@@ -0,0 +1,14 @@
+﻿using JjOnlineStore.Data.EF;
+using JjOnlineStore.Services.Business._Base;
+using JjOnlineStore.Services.Core;
+
+namespace JjOnlineStore.Services.Business
+{
+    public class ManageService : BaseService, IManageService
+    {
+        public ManageService(JjOnlineStoreDbContext dbContext) 
+            : base(dbContext)
+        {
+        }
+    }
+}
---
+++ Web/JjOnlineStore.Web/Controllers/ManageController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JjOnlineStore.Web.Controllers
+{
+    public class ManageController : BaseController
+    {
+
+    }
+}
