a4b6da3f23be128bb8a1cdd14f8e7b130e9e9499
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-01 12:45:05 +0200

    ready

---
+++ Web/JjOnlineStore.Web/Helpers/CalculateStoreOriginalPrice.cs
@@ -0,0 +1,8 @@
+﻿namespace JjOnlineStore.Web.Helpers
+{
+    public static class CalculateStoreOriginalPrice
+    {
+        public static decimal ComputeValue(decimal price) =>
+            price + price * 0.2m;
+    }
+}
