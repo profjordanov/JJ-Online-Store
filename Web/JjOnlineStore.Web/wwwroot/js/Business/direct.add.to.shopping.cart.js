5eb47a6ac3ad6e6516ba351971bea39254693101
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-01 12:03:47 +0200

    work in progress

---
+++ Web/JjOnlineStore.Web/wwwroot/js/Business/direct.add.to.shopping.cart.js
@@ -0,0 +1,28 @@
+﻿function directAddProductToCart(productId) {
+    const productQuantity = 1;
+
+    const cartItemBindingModel = {
+        productId: productId,
+        quantity: productQuantity
+    };
+
+    const request = {
+        method: "POST",
+        url: window.createCartItemUrl,
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(cartItemBindingModel)
+    };
+
+    $.ajax(request)
+        .then(displaySuccessMessage)
+        .catch(displayErrorMessage);
+}
+
+function displaySuccessMessage() {
+    $.fancybox("Product successfully added to your cart.");
+}
+
+function displayErrorMessage() {
+    $.fancybox("This product has already been added to the cart.");
+}
