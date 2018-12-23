ab7434f82c2f7a6a8bd8260b8cd082c5a19350f0
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-23 14:16:30 +0200

    init

---
+++ Services/JjOnlineStore.Services.Business/ImageStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using JjOnlineStore.Services.Core;
+
+namespace JjOnlineStore.Services.Business
+{
+    public class ImageStorageService : IImageStorageService
+    {
+        public async Task<string> StoreImage(string filename, byte[] image)
+        {
+            return null;
+        }
+    }
+}
