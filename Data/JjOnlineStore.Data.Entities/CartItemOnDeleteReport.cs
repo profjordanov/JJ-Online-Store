bcb41d95ca88b459fa12fa7f856ff8749032eaa6
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-23 22:43:00 +0200

    Update CartItemOnDeleteReport.cs

--- Data/JjOnlineStore.Data.Entities/CartItemOnDeleteReport.cs
+++ Data/JjOnlineStore.Data.Entities/CartItemOnDeleteReport.cs
@@ -2,6 +2,13 @@
 
 namespace JjOnlineStore.Data.Entities
 {
+    /// <summary>
+    /// This table will be filled when a Cart Item is deleted
+    /// via `TR_CartItems_DLT` trigger.
+    /// </summary>
+    /// <see>
+    ///     <cref>database-scripts.sql</cref>
+    /// </see>
     public class CartItemOnDeleteReport : BaseModel<int>
     {
         public long ProductId { get; set; }
