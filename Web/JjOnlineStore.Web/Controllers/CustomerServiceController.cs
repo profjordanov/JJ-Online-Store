58a1a852457dd53e75f315f91dfced825ac8bd8c
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-31 18:39:11 +0200

    init

--- Web/JjOnlineStore.Web/Controllers/CustomerServiceController.cs
+++ Web/JjOnlineStore.Web/Controllers/CustomerServiceController.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace JjOnlineStore.Web.Controllers
 {
+    [Route("customer-service/[action]")]
     public class CustomerServiceController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() =>
+            View();
     }
 }
