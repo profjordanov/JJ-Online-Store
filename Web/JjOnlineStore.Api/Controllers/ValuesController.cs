205ae4400601c09582fd667ff0d7221acdc8ba9d
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-03 23:51:22 +0200

    Update ValuesController.cs

--- Web/JjOnlineStore.Api/Controllers/ValuesController.cs
+++ Web/JjOnlineStore.Api/Controllers/ValuesController.cs
@@ -10,33 +10,9 @@ namespace JjOnlineStore.Api.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return "Welcome to JJ Online Store serverless API !" +
-                   " Author: Jordan Jordanov";
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+            return "Welcome to JJ Online Store serverless API ! " 
+                   + "The API is up and running! "
+                   + "Author: Jordan Jordanov";
+        }      
     }
 }
