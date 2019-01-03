a9cfe23c713abf243c4f30b35f6c815dcdea6db7
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-03 23:26:10 +0200

    Create ApiController.cs

---
+++ Web/JjOnlineStore.Api/Controllers/_Base/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using JjOnlineStore.Common.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JjOnlineStore.Api.Controllers._Base
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        protected IActionResult Error(Error error) =>
+            new BadRequestObjectResult(error);
+    }
+}
