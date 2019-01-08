896c82e63fd2de0d7212708901312289c4a59352
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-08 23:04:44 +0200

    init

---
+++ Web/JjOnlineStore.Web/Areas/Blog/Controllers/ArticlesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static JjOnlineStore.Common.GlobalConstants;
+
+namespace JjOnlineStore.Web.Areas.Blog.Controllers
+{
+    [Area(BlogArea)]
+    [Authorize(Roles = AdministratorRoleName)]
+    public class ArticlesController : Controller
+    {
+        /// GET: /Blog/Articles/Index
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public IActionResult Index() => View();
+    }
+}
